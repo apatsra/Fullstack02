05e55b9a079ef36ca3710a1990234a5f220b9bf1
apatsra <apatsra_pr@kkumail.com>
2022-08-19 01:52:42 +0700

    setup file

--- frontend-project/src/app/app.component.spec.ts
+++ frontend-project/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend-project'`, () => {
+  it(`should have as title 'front-end-simple-crud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-project');
+    expect(app.title).toEqual('front-end-simple-crud');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend-project app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('front-end-simple-crud app is running!');
   });
 });
